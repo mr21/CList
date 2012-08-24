3030ea53b78dc19592d82acc032c268e5bbc9e7e
Thomas Tortorini <thomastortorini@gmail.com>
2012-08-24 06:56:20 +0200

    src/CList_erase.c doit retourner le next (meme si c'est le dernier)

--- src/CList_erase.c
+++ src/CList_erase.c
@@ -10,7 +10,7 @@ CLink*		CList_erase(CList* li, CLink* ln)
   if (ln == li->begin)
     return CList_pop_front(li);
   if (ln == li->end)
-    return CList_pop_back(li);
+    return CList_pop_back(li), NULL;
   ln->prev->next = ln_ret = ln->next;
   ln->next->prev = ln->prev;
   --li->size;
