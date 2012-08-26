f3caf41dbec7cd1d4d450bde081aae11d80f50e2
Mr21 <thomastortorini@gmail.com>
2012-08-26 02:23:21 +0200

    CLink_prev/next prennent un size_t

--- src/CLink_getters.c
+++ src/CLink_getters.c
@@ -1,6 +1,23 @@
 #include	"CList.h"
 
-CLink*		CLink_prev(CLink const* ln) { return ln ? ln->prev : NULL; }
-CLink*		CLink_next(CLink const* ln) { return ln ? ln->next : NULL; }
-void*		CLink_data(CLink const* ln) { return ln ? ln->data : NULL; }
-void		(*CLink_free(CLink const* ln))() { return ln ? ln->free : NULL; }
+void*		CLink_data(CLink const* ln)
+{
+  return ln ? ln->data : NULL;
+}
+
+void		(*CLink_free(CLink const* ln))()
+{
+  return ln ? ln->free : NULL;
+}
+
+CLink*		CLink_prev(CLink const* ln, size_t n)
+{
+  for (; ln && n; ln = ln->prev, --n);
+  return (CLink*)ln;
+}
+
+CLink*		CLink_next(CLink const* ln, size_t n)
+{
+  for (; ln && n; ln = ln->next, --n);
+  return (CLink*)ln;
+}
